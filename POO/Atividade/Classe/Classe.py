15d5b8c489b189dbf55446ad230f9071474e1640
nicollas.sgama <n@gmail.com>
2023-10-05 11:20:12 -0300

    Classes e bancos

--- POO/Atividade/Classe/Classe.py
+++ POO/Atividade/Classe/Classe.py
@@ -8,14 +8,21 @@ class Animal:
         self.porte = porte
 
 
-class Cachorro(Animal):
-    def __init__(self, patas, pelagem, raca, nome, idade, cor, genero, especie, porte):
-        super().__init__(nome, idade, cor, genero, especie, porte)
-        self.patas = patas
-        self.pelagem = pelagem
-        self.raca = raca
+class Cliente:
+    def __init__(self, nome, email, cpf):
+        self.nome = nome
+        self.email = email
+        self.cpf = cpf
 
 
-A = Animal('Bob', '3', 'preto', 'masculino', 'canis familiaris', 'grande')
-print(A.nome)
-B = Animal
+# class Cachorro(Animal):
+#     def __init__(self, patas, pelagem, raca, nome, idade, cor, genero, especie, porte):
+#         super().__init__(nome, idade, cor, genero, especie, porte)
+#         self.patas = patas
+#         self.pelagem = pelagem
+#         self.raca = raca
+#
+#
+# A = Animal('Bob', '3', 'preto', 'masculino', 'canis familiaris', 'grande')
+# print(A.nome)
+# B = Animal
--- POO/Classe.py
+++ POO/Classe.py
@@ -1,22 +1,131 @@
-class Pessoa:
+from abc import ABC, abstractmethod
 
-    def __init__(self, *, nome, idade):
+
+# class Pessoa:
+#
+#     def __init__(self, *, nome, idade):
+#         self.nome = nome
+#         self.idade = idade
+#         self.nomeClasse = self.__class__.__name__
+#
+#     def falar(self):
+#         print(f"{self.nomeClasse} esta falando")
+#
+#
+# class Aluno(Pessoa):
+#     pass
+#
+#
+# class Professor(Pessoa):
+#     pass
+#
+#
+# aluno = Aluno("Carlos", 34)
+# professor = Professor("José", 55)
+# print(aluno.nome)
+
+
+class Conta(ABC):
+    def __init__(self, valor: float, cliente: str):
+        self.__saldo = valor  # Privar o parâmetro para que este só seja acessado por meio da classe
+        self.cliente = cliente
+        self.__status = True
+        self.__id = None
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @property
+    def status(self):
+        return self.__status
+
+    def alterar_status(self, status: bool):
+        self.__status = status
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo(self, valor: float):
+        self.__saldo -= valor
+
+    def deposito(self, valor: float):
+        if self.__status:
+            self.__saldo += valor
+        else:
+            raise 'A conta não está ativa'  # Força a tratar a sub-rotina com um try quando for chamada
+
+    @abstractmethod  # Força à classe que herdá-la a utilizar a função
+    def saque(self, valor: int):
+        pass
+
+
+class Conta_Corrente(Conta):
+
+    def __init__(self, valor: float, cliente: str, limite: float):
+        super().__init__(valor, cliente)
+        self.__limite = limite
+
+    def saque(self, valor: float):
+        if self.status:
+            if isinstance(valor, (int, float)):
+                if self.saldo >= valor:
+                    self.saldo = valor
+                elif self.saldo + self.__limite >= valor:
+                    self.saldo = self.saldo - valor
+                    self.__limite = self.__limite + self.saldo
+                    print('Você está no limite')
+                else:
+                    raise 'Saldo não cobre o valor de saque'
+            else:
+                raise 'Digite um valor válido'
+        else:
+            raise 'A conta não está ativa'
+
+
+class Conta_Poupaca(Conta):
+
+    def saque(self, valor: float):
+        if self.status:
+            if self.saldo >= valor:
+                self.saldo = valor
+            else:
+                raise 'Saldo não cobre o valor de saque'
+        else:
+            raise 'A conta não está ativa'
+
+
+cp = Conta_Poupaca(valor=1000, cliente='Pedrinho')
+cc = Conta_Corrente(valor=1400, cliente='Joselito', limite=1200)
+print(cp.saldo)
+cp.deposito(1000)
+print(cp.saldo)
+cp.saque(1999)
+print(cp.saldo)
+print('*' * 30)
+print(cc.saldo)
+cc.deposito(1573)
+print(cc.saldo)
+cc.saque(4000)
+print(cc.saldo)
+
+
+class Cliente:
+    def __init__(self, nome: str, nascimento: str):
         self.nome = nome
-        self.idade = idade
-        self.nomeClasse = self.__class__.__name__
+        self.nascimento = nascimento
+        self.__id = None
 
-    def falar(self):
-        print(f"{self.nomeClasse} esta falando")
+    @property
+    def id(self):
+        return self.__id
 
-
-class Aluno(Pessoa):
-    pass
-
-
-class Professor(Pessoa):
-    pass
-
-
-aluno = Aluno("Carlos", 34)
-professor = Professor("José", 55)
-print(aluno.nome)
+    @id.setter
+    def id(self, id):
+        self.__id = id
